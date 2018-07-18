b544ae916084edf7287a3be43d86a6cd8af5eaec
Alan Tran <truongprobk1992@gmail.com>
2018-07-18 14:38:31 +0700

    temporary remove react-native-parsed-text

--- src/MessageText.js
+++ src/MessageText.js
@@ -7,8 +7,6 @@ import {
   ViewPropTypes,
 } from 'react-native';
 
-import ParsedText from 'react-native-parsed-text';
-
 export default class MessageText extends React.Component {
   constructor(props) {
     super(props);
@@ -51,7 +49,7 @@ export default class MessageText extends React.Component {
   render() {
     return (
       <View style={[styles[this.props.position].container, this.props.containerStyle[this.props.position]]}>
-        <ParsedText
+        <Text
           style={[styles[this.props.position].text, this.props.textStyle[this.props.position]]}
           parse={[
             {type: 'url', style: StyleSheet.flatten([styles[this.props.position].link, this.props.linkStyle[this.props.position]]), onPress: this.onUrlPress},
@@ -60,7 +58,7 @@ export default class MessageText extends React.Component {
           ]}
         >
           {this.props.currentMessage.text}
-        </ParsedText>
+        </Text>
       </View>
     );
   }
